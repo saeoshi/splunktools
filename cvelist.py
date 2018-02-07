b57f3b3465daaab38c230b8548c620a6a730caa9
saeoshi <eswat.dunan@gmail.com>
2018-02-07 13:56:30 +0900

    cvedownloader create

---
+++ cvelist.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import urllib2
+
+def cvelist_get():
+    try:
+        response = urllib2.urlopen('http://jvndb.jvn.jp/myjvn\?method\=getVulnOverviewList').read()
+        print response
+
+#    except urllib2.HTTPError as e:
+#        print e.code
+#        print e.reason
+
+    except urllib2.URLError as e:
+        if hasattr(e, 'reason'):
+            print 'We failed to reach a server.'
+            print 'Reason: ', e.reason
+        elif hasattr(e, 'code'):
+            print 'The server couldn\'t fulfill the request.'
+            print 'Error code: ', e.code
+
+if __name__ == '__main__':
+    cvelist_get()
